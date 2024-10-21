bec76e753728c72dd09eff1523a7c4f096ebe9c2
terryweis <weist2088@gmail.com>
2024-10-21 02:08:19 -0500

    env file created

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./Config/db";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) =>{
-});
+app.get("/products", (req, res) =>{});
+
+console.log(process.env/MONGO_URI);
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 
 })
 
+//Kamasu18!
