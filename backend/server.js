0edcbb281a067f574b944d55dfbbe8bbc86f420d
terryweis <weist2088@gmail.com>
2025-01-14 23:54:38 -0600

    Added Api Endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,70 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import { connectDB } from "./Config/db.js";
+import Product from "./models/product.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) =>{});
+app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+console.log("error in fetching products:", error.message);
+res.status(500).json({ success: false, message: "Server Error"});
+    }
+})
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({ success:false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in Create product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error"});
+    }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+
+    const product = req.body;
+
+   if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid Product ID"})
+    }
+
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate(id,product,{new:true});
+        res.status(200).json({ success:true, data: updatedProduct});
+        } catch (error) {
+            res.status(500).json({ success: false, message: "Server Error"});
+        }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted" });
+    } catch (error) {
+        console.log("Error in Deleting Product");
+        res.status(404).json({ success: false, message: "Product Not Found" });
+    }
+})
 
 console.log(process.env.MONGO_URI);
 
